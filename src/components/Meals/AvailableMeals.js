b738a74665f4f81b04eb85cbbf7e50b3fc32be70
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-22 15:07:02 +0200

    :sparkles: Add meal items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
 import classes from './AvalilableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -30,11 +33,20 @@ const DUMMY_MEALS = [
 ];
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
