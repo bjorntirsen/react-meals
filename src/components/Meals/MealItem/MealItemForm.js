3c50930a34a68e3c06675c4ce09d8be85801828c
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-22 15:32:44 +0200

    :sparkles: Add form input

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Input from '../../UI/Input';
+
+import classes from './MealItemForm.module.css';
 
 const MealItemForm = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form className={classes.form}>
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          steps: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
