686c208e2ef86a52630c1f72f7f3d0ccb7cb85ad
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-23 14:01:13 +0200

    :sparkles: Manage open close modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,7 +4,7 @@ import Modal from '../UI/Modal';
 
 import classes from './Cart.module.css';
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
@@ -13,14 +13,14 @@ const Cart = () => {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>25.23</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button onClick={props.onClose} className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
